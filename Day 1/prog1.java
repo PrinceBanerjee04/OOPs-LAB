82ef2b3a228abaac36172944808df2392e5136fe
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-09-07 18:37:38 +0530

    Update prog1.java

--- Day 1/prog1.java
+++ Day 1/prog1.java
@@ -1,1 +1,10 @@
-
+class prog1
+{
+	public static void main(String[] args)
+	{
+		String name = "Prince";
+		int rollNo = 20221005;
+		System.out.println("Name : "+ name);
+		System.out.println("Roll No. " + rollNo);
+	}
+}
