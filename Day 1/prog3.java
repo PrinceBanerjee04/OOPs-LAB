a59eb14956a8ec9297ed73a70b1e7c04eb0fe9e1
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-09-09 00:36:10 +0530

    Update prog3.java

--- Day 1/prog3.java
+++ Day 1/prog3.java
@@ -1,1 +1,14 @@
-
+import java.util.*;
+class prog3
+{
+	public static void main(String[] args)
+	{
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter first number : ");
+		double a = in.nextDouble();
+		System.out.println("Enter second number : ");
+		double b = in.nextDouble();
+		double sum = a + b;
+		System.out.println("Sum = " + sum);
+	}
+}
