edefe33cd3476c7d63a9d4a1031dfb9f24b39bc8
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-09-08 19:56:15 +0530

    Update prog2.java

--- Day 1/prog2.java
+++ Day 1/prog2.java
@@ -1,1 +1,18 @@
+import java.util.*;
+class prog2
+{
+	public static void main(String[] args)
+	{
 
+		String roll = "20221005";
+		int last = Integer.parseInt(roll.substring(roll.length()-4));
+		int sum = 0;
+		while(last>0)
+		{
+			int lastD = last % 10;
+			sum = sum + lastD;
+			last/=10;
+		}
+		System.out.println("Sum = " + sum);
+	}
+}
