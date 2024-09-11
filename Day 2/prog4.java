295c803e4cf6aaed9a356af5b9df23d3c0a70099
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-09-11 22:22:55 +0530

    Update prog4.java

--- Day 2/prog4.java
+++ Day 2/prog4.java
@@ -3,7 +3,7 @@ a menu driven GUI based program in Java to perform the following task:
 2. subtraction
 3. multiplication
 4. division
- The user can perform the above tasks repetitively until he/she wants to quit.(use -99) to quit the program.
+The user can perform the above tasks repetitively until he/she wants to quit.(use -99) to quit the program.
 
 
 import javax.swing.JOptionPane;
