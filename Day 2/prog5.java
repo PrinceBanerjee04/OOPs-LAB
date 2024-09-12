4408b83e7357b7e592ec00a99b045c42a17c4063
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-09-12 21:44:15 +0530

    Update prog5.java

--- Day 2/prog5.java
+++ Day 2/prog5.java
@@ -1,1 +1,10 @@
-
+class prog5
+{
+	public static void main(String[] args)
+	{
+		boolean a = true;
+		boolean b = false;
+		System.out.println("Boolean values are : \n" + a);
+		System.out.println(b);
+	}
+}
