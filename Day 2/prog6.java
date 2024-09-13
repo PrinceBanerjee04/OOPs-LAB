d4ae0b90c2beeccc304ec37fbb029db3ea7692ed
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-09-14 00:03:37 +0530

    Update prog6.java

--- Day 2/prog6.java
+++ Day 2/prog6.java
@@ -1,1 +1,17 @@
-
+class prog6
+{
+	public static void main(String[] args)
+	{
+		int x = 5;
+		if(x==5)
+		{
+			int y = 5;
+			System.out.println("Value of x(inner scope) : " + x);
+			System.out.println("Value of y(inner scope) : " + y);
+			x--;
+		}
+		System.out.println("Value of x(outer scope) : " + x);
+	      //System.out.println("Value of y(outer scope) : " + y);
+	}
+}
+			
