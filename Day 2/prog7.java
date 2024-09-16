e0939cf33b7c560978c3f29d171fffd9409510cb
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-09-17 00:57:52 +0530

    Update prog7.java

--- Day 2/prog7.java
+++ Day 2/prog7.java
@@ -1,1 +1,15 @@
-
+import java.util.*;
+class prog7
+{
+	public static void main(String[] args)
+	{
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter a decimal number : ");
+		double d = in.nextDouble();
+		int i = (int) d;
+		byte b = (byte) i;
+		System.out.println("Value of double variable : "+ d);
+		System.out.println("Value of integer variable : "+ i);
+		System.out.println("Value of byte variable : "+ b);
+	}
+}
