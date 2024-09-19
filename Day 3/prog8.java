80e3d6613a72c27f04641d1e3939e787276a85d8
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-09-19 21:54:13 +0530

    Update prog8.java

--- Day 3/prog8.java
+++ Day 3/prog8.java
@@ -1,1 +1,12 @@
-
+class prog8
+{
+	public static void main(String[] args)
+	{
+		int arr[] = {10,20,30,40,50};
+		System.out.println("The array is : ");
+		for(int i=0;i<5;i++)
+		{
+			System.out.print(arr[i]+" ");
+		}
+	}
+}
