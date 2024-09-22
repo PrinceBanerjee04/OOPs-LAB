4743773e93cce6b933ef6e4eb26e580de57d11f0
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-09-23 00:10:27 +0530

    Update prog9.java

--- Day 3/prog9.java
+++ Day 3/prog9.java
@@ -1,1 +1,14 @@
 
+class prog9
+{
+	public static void main(String[] args)
+	{
+		int i, ls, rs;
+		i = 26;
+		ls = i<<1;
+		rs = i>>1;
+		System.out.println("Original Value : "+ i);
+		System.out.println("Value after left shift : "+ ls);
+		System.out.println("Value after right shift : " + rs);
+	}
+}
