9b98f56b39c81a85fac5a98eff9468da1f613e98
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-09-29 19:03:12 +0530

    Update prog10.java

--- Day 3/prog10.java
+++ Day 3/prog10.java
@@ -1,1 +1,9 @@
-
+class prog10
+{	
+	public static void main(String[] args)
+	{
+		int a = 6, b = 9;
+		int result = a < b ? a : b;
+		System.out.println("Minimum among "+ a + " and " + b + " is : "+ result);
+	}
+}
