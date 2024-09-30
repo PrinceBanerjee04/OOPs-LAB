efbc58d2570b3c7e23a566e41a2373a30973216f
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-09-30 20:53:19 +0530

    Update prog11.java

--- Day 3/prog11.java
+++ Day 3/prog11.java
@@ -1,1 +1,16 @@
-
+class prog11
+{	
+	public static void main(String[] args)
+	{
+		int a = 6, b = 9, min;
+		if(a < b)
+		{
+			min = a;
+		}
+		else
+		{
+			min = b;
+		}
+		System.out.println("Minimum value = " + min);
+	}
+}
