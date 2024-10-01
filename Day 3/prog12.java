33d5b8becfdf572969b2ec83b0ec8e5e96677755
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-10-01 09:50:46 +0530

    Update prog12.java

--- Day 3/prog12.java
+++ Day 3/prog12.java
@@ -1,1 +1,30 @@
-
+class prog12
+{	
+	public static void main(String[] args)
+	{
+		int a = 6, b = 9, c = 12, max;
+		if(a > b)
+		{
+			if(a > c)
+			{
+				max = a;
+			}
+			else
+			{
+				max = c;
+			}
+		}
+		else
+		{
+			if(b > c)
+			{
+				max = b;
+			}
+			else
+			{
+				max = c;
+			}
+		}
+		System.out.println("Maximum : " + max);
+	}
+}
