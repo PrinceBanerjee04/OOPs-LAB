32662e901fbe04b6d030d3f8344f7a672942eb1f
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-10-05 00:24:53 +0530

    Create prog8.java

--- Day 3/prog8.java
+++ Day 3/prog8.java
@@ -5,7 +5,7 @@ class prog8
 	public static void main(String[] args)
 	{
 		int arr[] = {10,20,30,40,50};
-		System.out.println("The array is : ");
+		System.out.print("The array is : \n");
 		for(int i=0;i<5;i++)
 		{
 			System.out.print(arr[i]+" ");
