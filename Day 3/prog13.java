a5af3dfae7ff3ad03a50a7e2b263803a1f1577ec
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-10-06 19:55:09 +0530

    Update prog13.java

--- Day 3/prog13.java
+++ Day 3/prog13.java
@@ -1,1 +1,14 @@
+import javax.swing.JOptionPane;
+class prog13
+{
+	public static void main(String[] args)
+	{
+		int i = 2;
+		while(i <= 6)
+		{
+			JOptionPane.showMessageDialog(null, i, "First three even numbers", JOptionPane.PLAIN_MESSAGE);
+			i+=2;
+		}
+	}
+}
 
