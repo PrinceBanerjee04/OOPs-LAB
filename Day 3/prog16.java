11693a145192dac4e80497d7a46fcf927fcfa306
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-10-15 00:18:52 +0530

    Update prog16.java

--- Day 3/prog16.java
+++ Day 3/prog16.java
@@ -1,1 +1,19 @@
 // Java program to show the use of continue.
+class prog16
+{
+	public static void main(String[] args)
+	{
+		int i, c = 0;
+		for(i = 0; i < 5; i++)
+		{
+			if(c==3)
+			{
+				System.out.println("The loop continued at : " + c);
+				c++;
+				continue;
+			}
+			System.out.println(i);
+			c++;
+		}
+	}
+}
