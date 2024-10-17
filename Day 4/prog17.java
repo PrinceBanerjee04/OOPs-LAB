9e6bf90aecd87ff92027877256fd784fe1641260
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-10-17 13:37:00 +0530

    Update prog17.java

--- Day 4/prog17.java
+++ Day 4/prog17.java
@@ -1,1 +1,21 @@
 Java to show the use of labelled break. 
+class prog17
+{
+	public static void main(String[] args)
+	{
+		label:{
+				for(int i=1;i<=10;i++)
+				{
+					for(int j=1;j<=i;j++)
+					{
+						if(i==6)
+						{
+							break label;
+						}
+						System.out.print("*");
+					}
+					System.out.println("");
+				}
+			}
+	}
+}
