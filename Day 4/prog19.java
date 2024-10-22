7c001969c05fe04933d5156f96b8942f903aa3c5
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-10-22 22:19:49 +0530

    Update prog19.java

--- Day 4/prog19.java
+++ Day 4/prog19.java
@@ -1,1 +1,15 @@
-
+class prog19
+{
+	public static void main(String[] args)
+	{
+		for(int i=1;i<=5;i++)
+		{
+			if(i==2)
+			{
+				System.out.println("The loop returned at : " + i);
+				return;
+			}
+			System.out.println(i);
+		}
+	}
+}
