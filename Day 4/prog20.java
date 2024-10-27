b5efc067ea7f23225f466051404b24decdc67300
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-10-27 18:28:02 +0530

    Update prog20.java

--- Day 4/prog20.java
+++ Day 4/prog20.java
@@ -1,1 +1,17 @@
+class prog20
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Area of rectangle : " + area(3,4));
+		System.out.println("Area of square : " + area(5));
+	}
+	public static int area(int l, int b)
+	{
+		return l*b;
+	}
+	public static int area(int s)
+	{
+		return s*s;
+	}	
 
+}
