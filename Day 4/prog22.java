434d37aeacde1e256a87a9403c75c5a6a7935eee
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-10-31 20:34:22 +0530

    Update prog22.java

--- Day 4/prog22.java
+++ Day 4/prog22.java
@@ -1,1 +1,16 @@
+class prog22
+{
+	public static void call(int i)
+	{
+		i+=10;
+	}
+	public static void main(String[] args)
+	{
+		int i = 10;
+		System.out.println("Value before call : " + i);
+		call(i);
+		System.out.println("Value after call : " + i);
+	}
+}
 
+		
