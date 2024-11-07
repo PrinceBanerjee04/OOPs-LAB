3bfc5edaddecb5555717946c150bf09a590bc552
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-11-08 02:18:25 +0530

    Update prog25.java

--- Day 5/prog25.java
+++ Day 5/prog25.java
@@ -1,1 +1,12 @@
 java to show the use of private, public, protected and default access specifiers.
+
+  public class prog25
+{
+	public static void main(String[] args)
+	{
+		p1.as1.display();
+		p1.as1a.display();
+		p2.as2.display();
+		p2.as2a.display();
+	}
+}
