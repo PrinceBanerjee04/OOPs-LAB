1c776a9e277b4c476ac14b6f9947b93eab1fb5db
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-11-09 01:44:03 +0530

    Update prog25.java

--- Day 5/prog25.java
+++ Day 5/prog25.java
@@ -1,5 +1,21 @@
 // Java program to show the use of private, public, protected and default access specifiers.
-
+package p1;
+public class as1
+{
+	public String pubA = "Public Variable";
+	private String privA = "Private Variable";
+	protected String procA = "Protected Variable";
+	String defA = "Default Variable";
+	public static void display()
+	{
+		as1 ob = new as1();
+		System.out.println("Same package and same class : ");
+		System.out.println("Public access : " + ob.pubA);
+		System.out.println("Private access : " + ob.privA);
+		System.out.println("Protected access : " + ob.procA);
+		System.out.println("Default access : " + ob.defA + "\n");
+	}
+}
 	
 public class prog25
 {
