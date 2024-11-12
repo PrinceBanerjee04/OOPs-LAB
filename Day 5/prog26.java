786228ff83801c94f83063c4edeccb224d7bc602
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-11-12 10:29:05 +0530

    Update prog26.java

--- Day 5/prog26.java
+++ Day 5/prog26.java
@@ -1,2 +1,16 @@
 
 
+class sMethod
+{
+	public static void area(int l,int b)
+	{
+		System.out.println("Area : " + (l*b));
+	}
+}
+class prog26
+{
+	public static void main(String[] args)
+	{
+		sMethod.area(4,5);
+	}
+}
