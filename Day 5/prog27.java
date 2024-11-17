3e4a3b368eaacb5810912b8d2d461b7285d4db20
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-11-17 12:20:51 +0530

    Update prog27.java

--- Day 5/prog27.java
+++ Day 5/prog27.java
@@ -1,3 +1,21 @@
 Java to show the use of static variable.
 
   
+class count
+{
+	static int c = 0;
+	count()
+	{
+		c++;
+	}
+}
+class prog27
+{
+	public static void main(String[] args)
+	{
+		count ob1 = new count();
+		count ob2 = new count();
+		System.out.println("Total objects created : " + count.c);
+	}
+}
+		
