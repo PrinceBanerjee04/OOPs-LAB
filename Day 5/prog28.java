e68b9b3591205ff27d4cd86ef54f40d02a7e1f2b
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-11-20 02:17:52 +0530

    Update prog28.java

--- Day 5/prog28.java
+++ Day 5/prog28.java
@@ -1,1 +1,25 @@
-
+class animal
+{
+	String name;
+	public void eat()
+	{
+		System.out.println("Eats!!");
+	}
+}
+class dog extends animal
+{
+	public void display()
+	{
+		System.out.println("My name is : " + name);
+	}
+}
+class prog28
+{
+	public static void main(String[] args)
+	{
+		dog ob = new dog();
+		ob.name = "Cat";
+		ob.display();
+		ob.eat();
+	}
+}
