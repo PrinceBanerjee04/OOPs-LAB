6fd75cc0c8de332b8b83e38f87faa530c476a154
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-11-24 21:43:26 +0530

    Update prog29.java

--- Day 5/prog29.java
+++ Day 5/prog29.java
@@ -1,2 +1,25 @@
  java to show the use of super.
 
+class animal
+{
+	public void eat()
+	{
+		System.out.println("Animal Eats");
+	}
+}
+class dog extends animal
+{
+	public void eat()
+	{
+		super.eat();
+		System.out.println("Dog eats");
+	}
+}
+class prog29
+{
+	public static void main(String[] args)
+	{
+		dog ob = new dog();
+		ob.eat();
+	}
+}
