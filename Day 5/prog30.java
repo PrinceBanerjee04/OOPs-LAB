97c51d3387255b8928f5e2e632334da00d91579a
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-11-26 18:08:20 +0530

    Update prog30.java

--- Day 5/prog30.java
+++ Day 5/prog30.java
@@ -1,2 +1,26 @@
 // Java program to show method overriding.
 
+class animal
+{
+	public void eat()
+	{
+		System.out.println("Starting to eat");
+	}
+}
+class dog extends animal
+{
+	public void eat()
+	{
+		System.out.println("Finished eating");
+	}
+}
+class prog30
+{
+	public static void main(String[] args)
+	{
+		animal ob1 = new animal();
+		dog ob2 = new dog();
+		ob1.eat();
+		ob2.eat();
+	}
+}
