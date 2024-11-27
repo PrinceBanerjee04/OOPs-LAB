408fc8ae634e78eb430c984ef6165cdf6e79750b
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-11-27 19:02:15 +0530

    Update prog31.java

--- Day 5/prog31.java
+++ Day 5/prog31.java
@@ -1,1 +1,27 @@
-
+class A
+{
+	public void print()
+	{
+		System.out.println("Inside class A");
+	}
+}
+class B extends A
+{
+	public void print()
+	{
+		System.out.println("Inside class B");
+	}
+}
+class prog31
+{
+	public static void main(String[] args)
+	{
+		A a = new A();
+		B b = new B();
+		A r;
+		r = a;
+		r.print();
+		r = b;
+		r.print();
+	}
+}
