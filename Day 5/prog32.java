714eb55344d44509f47e874e5ac6900f6db6e8fa
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-11-29 11:15:57 +0530

    Update prog32.java

--- Day 5/prog32.java
+++ Day 5/prog32.java
@@ -1,1 +1,20 @@
-
+abstract class Animal
+{
+	abstract void eat();
+}
+class Dog extends Animal
+{
+	void eat()
+	{
+		System.out.println("Dog eats.");
+	}
+}
+class prog32
+{
+	public static void main(String[] args)
+	{
+		Animal ob = new Dog();
+		ob.eat();
+	}
+}
+		
