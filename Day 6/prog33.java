9724cd3c2543fb8ca356680ea2a243ef0bed6477
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-12-02 01:17:28 +0530

    Update prog33.java

--- Day 6/prog33.java
+++ Day 6/prog33.java
@@ -4,6 +4,11 @@ public class prog33a
 	public static String a = "Inside class of First Package";
 }
 
+package p2;
+public class prog33b
+{
+	public static String b = "Inside class of Second Package";
+}
 class prog33
 {
 	public static void main(String[] args)
