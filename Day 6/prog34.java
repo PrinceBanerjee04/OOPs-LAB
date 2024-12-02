82f0841f8ffe2ab5413c5449e0df6f2a763109d7
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-12-03 00:09:40 +0530

    Update prog34.java

--- Day 6/prog34.java
+++ Day 6/prog34.java
@@ -1,1 +1,16 @@
-
+interface Animal
+{
+	void eat();
+}
+class prog34 implements Animal
+{
+	public void eat()
+	{
+		System.out.println("Animal Eats!!");
+	}
+	public static void main(String[] args)
+	{
+		prog34 ob = new prog34();
+		ob.eat();
+	}
+}
