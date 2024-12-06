1ef2296388eaa0c5ed0c5e2ddf0405fa048b3884
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-12-06 14:03:32 +0530

    Update prog35.java

--- Day 6/prog35.java
+++ Day 6/prog35.java
@@ -1,2 +1,27 @@
 //Java program to implement multiple inheritances.
 
+interface Animal
+{
+	void eat();
+}
+abstract class Dog
+{
+	abstract void sleep();
+}
+class prog35 extends Dog implements Animal
+{
+	public void eat()
+	{
+		System.out.println("Animal Eats!!");
+	}
+	public void sleep()
+	{
+		System.out.println("Dog Sleeps!!");
+	}
+	public static void main(String[] args)
+	{
+		prog35 ob = new prog35();
+		ob.eat();
+		ob.sleep();
+	}
+}
