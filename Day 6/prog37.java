2d96cd3260bcefa53344ca48d58634a1c427856e
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-12-08 00:15:49 +0530

    Create prog37.java

--- Day 6/prog37.java
+++ Day 6/prog37.java
@@ -1,2 +1,16 @@
 // Java program to show the use of ArithmaticException.
 
+class prog37
+{
+	public static void main(String[] args)
+	{
+		int d, a;
+ 		try { 
+ 			d = 0;
+ 			a = 42 / d;
+ 		} catch (ArithmeticException e) {
+ 			System.out.println("Division by zero.");
+ 		}
+ 		System.out.println("After catch statement.");
+	}
+}
