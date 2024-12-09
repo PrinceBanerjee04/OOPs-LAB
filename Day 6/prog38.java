f153086f151074769ed3c67a47cf5a69c64b4d68
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-12-10 00:04:02 +0530

    Update prog38.java

--- Day 6/prog38.java
+++ Day 6/prog38.java
@@ -1,1 +1,21 @@
-
+import java.util.*;
+class prog38
+{
+	public static void main(String[] args)
+	{
+		Scanner in = new Scanner(System.in);
+		try {
+			System.out.println("Enter divisor value : ");
+ 			int d = in.nextInt();
+ 			System.out.println("d = " + d);
+ 			int b = 42 / d;
+ 			int c[] = { 1, 2, 3, 4, 5 };
+ 			c[42] = 99;
+ 		} catch(ArithmeticException e) {
+ 			System.out.println("Result : " + e);
+ 		} catch(ArrayIndexOutOfBoundsException e) {
+ 			System.out.println("Array index: " + e);
+ 		}
+ 		System.out.println("After try/catch blocks.");
+	}
+}
