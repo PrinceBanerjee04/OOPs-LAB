e9ee567b4c74d6e4cfa813651770a1345cefba9a
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-12-11 01:40:50 +0530

    Update prog39.java

--- Day 6/prog39.java
+++ Day 6/prog39.java
@@ -1,1 +1,25 @@
+import java.util.*;
+class prog39
+{
+	public static void main(String[] args)
+	{
+		Scanner in = new Scanner(System.in);
+		try {
+			System.out.println("Enter divisor value : ");
+ 			int d = in.nextInt();
+ 			int a = 42 / d;
+			try {
+				if(d==2) 
+				{
+ 					int c[] = { 1 };
+ 					c[42] = 99;
+ 				}
+ 			} catch(ArrayIndexOutOfBoundsException e) {
+ 				System.out.println("Array index: " + e);
+ 			}
+ 		} catch(ArithmeticException e) {
+ 			System.out.println("Division by zero: " + e);
+ 		}
+	}
+}
 
