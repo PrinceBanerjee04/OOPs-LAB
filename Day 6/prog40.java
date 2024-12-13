63399dbff9abf6ca2a6405b145cba92ce8322c73
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-12-13 23:04:55 +0530

    Update prog40.java

--- Day 6/prog40.java
+++ Day 6/prog40.java
@@ -1,1 +1,17 @@
+class prog41
+{
+	static void THROW() throws IllegalAccessException 
+	{
+ 		System.out.println("Inside");
+ 		throw new IllegalAccessException("demo");
+ 	}
+ 	public static void main(String args[]) 
+	{
+ 		try {
+ 			THROW();
+ 		} catch (IllegalAccessException e) {
+ 			System.out.println("Caught " + e);
+ 		}
+ 	}
+}
 
