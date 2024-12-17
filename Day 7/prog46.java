faa50b96132356c085bc0e7bd976bcc17260f295
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-12-17 16:49:08 +0530

    Update prog46.java

--- Day 7/prog46.java
+++ Day 7/prog46.java
@@ -1,3 +1,4 @@
+when you are calculating the summation of numbers by two threads (assignment no 42) the time requirement is less than the time requirement by a single threaded program for doing the same task.
 
 
 class Sum extends Thread
