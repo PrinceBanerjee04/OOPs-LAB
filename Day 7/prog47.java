7f124c37c255614f618c3ab88d8579007c52b5b5
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-12-20 00:05:09 +0530

    Update prog47.java

--- Day 7/prog47.java
+++ Day 7/prog47.java
@@ -1,4 +1,4 @@
-Java which will take input from the command prompt and show that on screen.
+Java program which will take input from the command prompt and show that on screen.
 
 import java.io.*;
 class prog47
