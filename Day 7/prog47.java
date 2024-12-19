9d2817d9c9faaa9dfc5db23e8985ce364074b46f
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-12-20 00:03:54 +0530

    Update prog47.java

--- Day 7/prog47.java
+++ Day 7/prog47.java
@@ -1,1 +1,14 @@
-
+import java.io.*;
+class prog47
+{
+	public static void main(String[] args) throws IOException
+	{
+		char ch;
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.print("Enter character, ! to quit : ");
+		do {
+			ch = (char)br.read();
+			System.out.println(ch);
+		} while(ch != '!');
+	}
+}
