76e2990ece31bae3f03f3aaeed8e4a8db9b5ea22
Prince Banerjee <130071684+PrinceBanerjee04@users.noreply.github.com>
2024-12-23 18:21:46 +0530

    Update prog49.java

--- Day 7/prog49.java
+++ Day 7/prog49.java
@@ -1,1 +1,65 @@
+import java.util.*;
+class Stack<T> 
+{
+	private ArrayList<T> elements;
+	public Stack() 
+	{
+		elements = new ArrayList<>();
+    	}
 
+    	public void push(T element) 
+	{
+        	elements.add(element);
+    	}
+    	public T pop() 
+	{
+        	if (isEmpty()) 
+		{
+            		throw new IllegalStateException("Stack is empty. Cannot pop.");
+        	}
+        	return elements.remove(elements.size() - 1);
+    	}
+    	public T peek() 
+	{
+        	if (isEmpty()) 
+		{
+            		throw new IllegalStateException("Stack is empty. Cannot peek.");
+        	}
+        	return elements.get(elements.size() - 1);
+    	}
+
+    	public boolean isEmpty()
+	{
+        	return elements.isEmpty();
+    	}
+
+    	public int size() 
+	{
+        	return elements.size();
+    	}
+}
+class prog49
+{
+	public static void main(String[] args) 
+	{
+        	Stack<Integer> intStack = new Stack<>();
+        	Stack<String> stringStack = new Stack<>();
+
+        	intStack.push(10);
+        	intStack.push(20);
+        	intStack.push(30);
+
+        	System.out.println("Integer Stack Size: " + intStack.size());
+        	System.out.println("Top Element: " + intStack.peek());
+        	System.out.println("Popped Element: " + intStack.pop());
+        	System.out.println("Stack After Pop: " + intStack.size());
+
+		stringStack.push("Hello");
+        	stringStack.push("World");
+
+        	System.out.println("String Stack Size: " + stringStack.size());
+        	System.out.println("Top Element: " + stringStack.peek());
+        	System.out.println("Popped Element: " + stringStack.pop());
+        	System.out.println("Stack Empty : " + stringStack.isEmpty());
+	}
+}
